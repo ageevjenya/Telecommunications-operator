71feb0f9412d4c0af6a9858be13242e066776bce
Eldaruss <Eldar.sc@yandex.ru>
2020-10-20 19:32:08 +0300

    Fix buy tariffs

--- src/main/java/com/netcracker/app/domain/Application.java
+++ src/main/java/com/netcracker/app/domain/Application.java
@@ -1,13 +1,9 @@
 package com.netcracker.app.domain;
-
-import com.netcracker.app.domain.info.entities.contacts.ContactsImpl;
-import com.netcracker.app.domain.info.services.contacts.ContactsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/netcracker/app/domain/tariffs/controllers/TariffsHomeController.java
+++ src/main/java/com/netcracker/app/domain/tariffs/controllers/TariffsHomeController.java
@@ -129,20 +129,29 @@ public class TariffsHomeController {
     }
 
     @PostMapping("/connect")
-    public String userСonnectTariff(@RequestParam("tariffHomeId") Long tariffHomeId, Model model) throws Exception {
+    public String userConnectTariff(@RequestParam("tariffHomeId") Long tariffHomeId, Model model) throws Exception {
         User user = userRepo.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
         if (user == null) {
             return "login";
         }
 
+        String description;
         TariffHome tariffHome = tariffHomeRepository.getOne(tariffHomeId);
+
+        try {
+            balanceService.updateBalance(tariffHome.getPriceOfMonth(), user.getBalance().getId());}
+        catch (Exception e ){
+            description = "Недостатчоно средств";
+            notificationsServiсe.AddNewNotificationInBDonDesctiption(description);
+            return "redirect:/tariffsHome";
+        }
+
         user.setTariffHome(tariffHome);
         userRepo.save(user);
-        String description = "Вы подключили тариф: " + tariffHome.getName() + "\n" + tariffHome.getDescription();
-        balanceService.updateBalance(tariffHome.getPriceOfMonth(), user.getBalance().getId());
-        Expenses expenses = expensesService.getById(new Expenses().getId());
-        expensesService.updateExpenses(user.getTariffMobile().getPriceOfMonth(), new GregorianCalendar(), "Домашний интернет", expenses.getId(), user);
+        description = "Вы подключили тариф: " + tariffHome.getName() + "\n" + tariffHome.getDescription();
         notificationsServiсe.AddNewNotificationInBDonDesctiption(description);
         return "redirect:/tariffsHome";
+
+
     }
 }
